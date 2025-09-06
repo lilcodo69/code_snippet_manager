cc527db7efbd7db03f3b05b0b00b5c551f1b27d4
Suyash Upadhyay <uranium235.a@gmail.com>
2025-09-06 15:52:42 +0530

    fix :AuthLayout

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -1,25 +1,23 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+// src/components/AuthLayout.tsx
+
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext"; 
 
 const AuthLayout = () => {
   const { session, isLoading: isLoadingSession } = useAuth();
 
-  const navigate = useNavigate();
-
-  if (!session) {
-    navigate("/login");
-  } else {
-    if (window.location.pathname === "/login") {
-      navigate("/");
-    }
-  }
 
   if (isLoadingSession) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-        Loading user session...
+        Loading...
       </div>
     );
+  }
+
+
+  if (!session) {
+    return <Navigate to="/login" replace />;
   }
 
   return <Outlet context={{ session }} />;
